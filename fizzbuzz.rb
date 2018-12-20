33396e2b456524329586b9e58be22baab921906d
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-20 15:45:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    return "Fizz" # Go fizz
+  if int % 3 == 0 and int % 5 == 0
+    return "FizzBuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 and int % 5 == 0
-    return "FizzBuzz"
-  end
-end
+  elsif int % 3 == 0
+    return  "Fizz"
+
+   end
+
+ end
